0dba09bb4c8a27283514b846fddef0cd234c075c
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-07-24 11:00:44 -0700

    Updated HomController.cs

---
+++ Tracker/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tracker.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+    }
+}
